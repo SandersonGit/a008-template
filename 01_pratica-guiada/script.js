e6a445e8228d816a9be1709a21852d49ee499cd7
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-07-20 21:30:34 -0300

    ex Variáveis e operadores

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,89 @@
 //CODIGO AQUI
+
+console.log("ok")
+
+// operadores
+
+let saldo = 15 + 22.40
+console.log("saldo inicial:",saldo);
+saldo = saldo - 12.34
+console.log("Saldo subtraído:", saldo);
+saldo = saldo / 3
+console.log("parte por irmão:", saldo);
+saldo = saldo * 3.42
+console.log("parte investida:", saldo);
+saldo = saldo % 3
+console.log("Sobrou:", saldo);
+
+// ordem
+
+const multiSoma = 2*2+2
+console.log(multiSoma);
+
+const somaMulti = 2*(2+2)
+console.log(somaMulti);
+
+// o javascript respeita as regras aritimética
+
+const conta = 5*3+42
+console.log(conta);
+
+const conta1 = 5*(15+33)
+console.log(conta1);
+
+const conta2 = 83-(35*3)+43*(11+145)
+console.log(conta2);
+
+// true ou false (boleano)
+// crie uma variavel para guardar cada resultado
+
+
+let compIgual = 5===5
+console.log("compIgual =", compIgual);
+
+let compDif = 5!==5
+console.log("compDif =", compDif);
+
+let compTipo = 5!=="5"
+console.log("compTipo =", compTipo);
+
+// comparação de valores == (igualdade)
+// comparação de valores e tipo === (igualdade)
+// comparação de valores != (igualdade)
+// comparação de valores e tipo !== (igualdade)
+
+let compTipo1 = "5"==="cinco"
+console.log("compTipo1 =", compTipo1);
+
+let compTipo2 = typeof 5 === typeof 5
+console.log("compTipo2", compTipo2);
+
+// exercícios de fixação
+
+/* const a = 13
+const b = 20 */
+
+let resultado
+
+resultado = a==b
+console.log("O primeiro número é igual ao segundo? -", resultado);
+
+resultado = a<=b
+console.log("O primeiro número é igual ou menor que o segundo? -", resultado);
+
+resultado = a>b
+console.log("O primeiro número é maior que o segundo? -", resultado);
+
+resultado = a<b
+console.log("O primeiro número é menor que o segundo? -", resultado);
+
+// se , senão
+
+let a = "quero dirigir um celta 2012"
+let b = "sou maior de idade"
+let c = "Consigo comprar um celta por 22.000 reais"
+let d = "consigo alugar um celta"
+let e = ""
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,17 @@
 // EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+const minhaIdade = Number(prompt('Qual sua idade?'))
+const idadeAmiga = Number(prompt('Qual a idade da sua amiga ou amigo?'))
+
+console.log("Sua idade é maior que a da sua melhor amiga? -", minhaIdade>idadeAmiga);
+console.log("A diferença de idade é de: ", minhaIdade-idadeAmiga, "anos!");
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+!b && d
+b && a && !d
+a || d
+c && !d
